665ea97b6b9759f3ad3df89f9e3fd825242a4392
asdacosta <asdacosta18@gmail.com>
2023-11-15 23:15:11 +0000

    Add color change feature

---
+++ src/setting.js
@@ -0,0 +1,59 @@
+const generateColors = (function () {
+    const red = 'rgba(253, 111, 85, 0.7)';
+    const yellow = 'rgba(252, 252, 106, 0.7)';
+    const blue = 'rgba(115, 210, 253, 0.7)';
+    const violet = 'rgba(250, 80, 250, 0.7)';
+    const grey = 'rgba(158, 158, 158, 0.7)';
+    const wood = 'rgba(223, 179, 123, 0.7)';
+
+    return {red, yellow, blue, violet, grey, wood};
+})();
+
+const paintColorCircle = function () {
+    const colorCircles = document.querySelectorAll('.colorContainer > span');
+
+    colorCircles.forEach((circle) => {
+        switch(circle.className) {
+            case 'colorCircle0':
+                circle.style.background = `${generateColors.red}`;
+                break;
+            case 'colorCircle1':
+                circle.style.background = `${generateColors.yellow}`;
+                break;
+            case 'colorCircle2':
+                circle.style.background = `${generateColors.blue}`;
+                break;
+            case 'colorCircle3':
+                circle.style.background = `${generateColors.violet}`;
+                break;
+            case 'colorCircle4':
+                circle.style.background = `${generateColors.grey}`;
+                break;
+            case 'colorCircle5':
+                circle.style.background = `${generateColors.wood}`;
+                break;
+        }
+    })
+};
+
+const displaySetting = function () {
+
+    const header = document.querySelector('h2');
+    header.textContent = 'Settings';
+
+    const colorChangeText = document.createElement('p');
+    colorChangeText.textContent = 'Choose your desired color:';
+
+    const colorContainer = document.createElement('div');
+    colorContainer.classList.add('colorContainer');
+    for (let m = 0; m < 6; m++) {
+        const colorCircle = document.createElement('span');
+        colorCircle.classList.add(`colorCircle${m}`);
+        colorContainer.appendChild(colorCircle);
+    }
+
+    header.insertAdjacentElement('afterend', colorContainer);
+    header.insertAdjacentElement('afterend', colorChangeText);
+};
+
+export {displaySetting, paintColorCircle, generateColors};
